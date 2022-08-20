b32e1aa6439db111b26f6fab7041ce2798a82149
Jon Moore <jonmoore510@gmail.com>
2022-08-20 13:08:06 -0500

    updating asset paths after parcel integration

--- src/game.ts
+++ src/game.ts
@@ -1,16 +1,30 @@
 import "phaser";
 import planets from "./assets/images/kenney_planets/Planets/*.png";
+import ships from "./assets/images/kenney_spaceshooterextension/PNG/Sprites/Ships/*.png";
+import { Position } from "./types";
 import GameConfig = Phaser.Types.Core.GameConfig;
 
 export default class Game extends Phaser.Scene {
   private width: number;
   private height: number;
-  private sceneId = "game";
+  private screenCenter: Position;
+
+  // @ts-ignore
+  private homeWorld: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  private planetScaleFactor: number;
+
+  // @ts-ignore
+  private ship: SpriteWithDynamicBody;
+  private shipScaleFactor: number;
 
   constructor() {
-    super(game);
+    super("game");
     this.width = window.innerWidth;
     this.height = window.innerHeight;
+    this.screenCenter = { x: window.innerWidth, y: window.innerHeight };
+
+    this.planetScaleFactor = 0.1;
+    this.shipScaleFactor = 0.5;
   }
 
   init() {}
@@ -18,11 +32,19 @@ export default class Game extends Phaser.Scene {
   preload() {
     // load assets here
     this.load.image("homeWorld", planets.planet00);
+
+    this.load.image("ship", ships.spaceShips_005);
   }
 
   create() {
     //  add to top-level game object
-    this.physics.add.sprite(this.width / 2, this.height / 2, "homeWorld");
+    const { x, y } = this.screenCenter;
+    this.homeWorld = this.physics.add.sprite(x / 2, y / 2, "homeWorld");
+
+    this.homeWorld.setScale(this.planetScaleFactor);
+
+    this.ship = this.physics.add.sprite(x - 200, y - 200, "ship");
+    this.ship.setScale(this.shipScaleFactor);
   }
 }
 
